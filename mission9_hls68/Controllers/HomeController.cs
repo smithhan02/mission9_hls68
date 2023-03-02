c7a357d5f1c7ca0fc366263184a91828ab0eb808
Hannah Smith <hls68@byu.edu>
2023-03-01 23:20:13 -0700

    Add project files.

---
+++ mission9_hls68/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using mission9_hls68.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mission9_hls68.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BookstoreContext context { get; set; }
+
+        public HomeController (BookstoreContext temp)
+        {
+            context = temp;
+        }
+
+        public IActionResult Index()
+        {
+            var blah = context.Books.ToList();
+
+            return View();
+        }
+
+        
+    }
+}
